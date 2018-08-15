a9bd6dec88b09e35e8790ef758f88849f598506a
Gustavo <gplazar03@gmail.com>
2018-08-15 21:16:23 +0200

    debug php

--- php/process-form.php
+++ php/process-form.php
@@ -1,12 +1,15 @@
 <?php
 if (isset($_REQUEST['name'],$_REQUEST['email'])) {
       
+    $myvar = array(1,2,3);
+    console_log( $myvar ); // [1,2,3]
+
     $name = $_REQUEST['name'];
     $email = $_REQUEST['email'];
     $message = $_REQUEST['message'];
       
     // Set your email address where you want to receive emails. 
-    $to = 'gplazar03@gmail.com';
+    $to = 'youremail@mail.com';
       
     $subject = 'Contact Request From Website';
     $headers = "From: ".$name." <".$email."> \r\n";
@@ -16,4 +19,12 @@ if (isset($_REQUEST['name'],$_REQUEST['email'])) {
     echo ($send_email) ? 'success' : 'error';
       
 }
+
+
+function console_log( $data ){
+  echo '<script>';
+  echo 'console.log('. json_encode( $data ) .')';
+  echo '</script>';
+}
+
 ?>
